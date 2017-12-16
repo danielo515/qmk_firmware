edb234e9ab478698da156a77b59c9660551cce32
Danielo Rodriguez Rivero <rdanielo@gmail.com>
2017-12-16 22:12:13 +0100

    fix(jj40): Added taping term

--- keyboards/jj40/config.h
+++ keyboards/jj40/config.h
@@ -30,6 +30,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COLS 15
 
 #define TAPPING_TOGGLE 3
+#define TAPPING_TERM 200
 
 #define NO_UART 1
 #define BOOTLOADHID_BOOTLOADER 1
