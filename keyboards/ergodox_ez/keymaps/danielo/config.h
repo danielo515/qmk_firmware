857270d94c99420b4d4fdc2d1b22e543ddc1b0cc
Danielo Rodriguez Rivero <rdanielo@gmail.com>
2018-12-02 20:17:46 +0100

    feat(danielo): added the basic danielo ergodox layout

---
+++ keyboards/ergodox_ez/keymaps/danielo/config.h
@@ -0,0 +1,15 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#undef TAPPING_TERM
+#define TAPPING_TERM 240
+
+#define ONESHOT_TAP_TOGGLE 2
+
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX 50
+
+#undef MOUSEKEY_WHEEL_TIME_TO_MAX
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 60
+
