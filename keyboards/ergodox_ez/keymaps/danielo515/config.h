49cb754a19d43b2f8ff49c6440881f77d06613a8
Danielo Rodriguez <rdanielo@gmail.com>
2019-05-24 08:29:52 +0200

    chore: small adjustments

--- keyboards/ergodox_ez/keymaps/danielo515/config.h
+++ keyboards/ergodox_ez/keymaps/danielo515/config.h
@@ -3,7 +3,7 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 #undef TAPPING_TERM
-#define TAPPING_TERM 250
+#define TAPPING_TERM 200
 
 #define ONESHOT_TAP_TOGGLE 2
 
@@ -16,7 +16,7 @@
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 60
 
 #undef  LEADER_TIMEOUT
-#define LEADER_TIMEOUT 250
+#define LEADER_TIMEOUT 350
 #define LEADER_PER_KEY_TIMING
 
 #undef NO_DEBUG
