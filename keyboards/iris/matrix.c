801e80affa79a7ce58da04a8cb2395dfa762493e
Danielo Rodriguez <rdanielo@gmail.com>
2020-02-08 05:39:25 +0100

    fix(iris): do not include backlight by default

--- keyboards/iris/matrix.c
+++ keyboards/iris/matrix.c
@@ -30,7 +30,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "pro_micro.h"
 #include "config.h"
 #include "timer.h"
-#include "backlight.h"
+#ifdef BACKLIGHT_ENABLE
+        #include "backlight.h"
+#endif
 
 #ifdef USE_I2C
 #  include "i2c.h"
