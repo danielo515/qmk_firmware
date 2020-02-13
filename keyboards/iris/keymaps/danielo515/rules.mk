662a944b0f3112d23e98bfb6cd6c2e15c3d0ce99
Danielo Rodriguez <rdanielo@gmail.com>
2020-02-13 09:04:33 +0100

    feat: convert more keycodes to iris ones

--- keyboards/iris/keymaps/danielo515/rules.mk
+++ keyboards/iris/keymaps/danielo515/rules.mk
@@ -1,8 +1,9 @@
 RGBLIGHT_ENABLE = no
-BACKLIGHT_ENABLE = no
+BACKLIGHT_ENABLE = yes
 TAP_DANCE_ENABLE = yes
 MOUSEKEY_ENABLE = no       # Mouse keys(+4700)
 KEY_LOCK_ENABLE = no
+COMBO_ENABLE = yes
 
 ifndef QUANTUM_DIR
 	include ../../../../Makefile
