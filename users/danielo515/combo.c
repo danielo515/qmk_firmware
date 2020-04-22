6edac1b1e99bed950a30b7f2ccce4a837c165b68
Danielo Rodriguez <rdanielo@gmail.com>
2020-04-22 09:59:53 +0200

    chore: lower combo time and some more combos

--- users/danielo515/combo.c
+++ users/danielo515/combo.c
@@ -6,7 +6,7 @@ enum combos {
     UI_COM,
     IO_COM,
     QW_COM,
-    COM_SLS,
+    DOT_SLS,
     COM_DOT,
     M_COMM,
     N_M,
@@ -18,7 +18,7 @@ const uint16_t PROGMEM jk_combo[] = {KC_J, KC_K, COMBO_END};
 const uint16_t PROGMEM yu_combo[] = {KC_Y, KC_U, COMBO_END};
 const uint16_t PROGMEM io_combo[] = {KC_I, KC_O, COMBO_END};
 const uint16_t PROGMEM qw_combo[] = {KC_Q, KC_W, COMBO_END};
-const uint16_t PROGMEM com_sls[] = {KC_COMMA, KC_SLSH, COMBO_END};
+const uint16_t PROGMEM dot_sls[] = {KC_DOT, KC_SLSH, COMBO_END};
 const uint16_t PROGMEM com_dot[] = {KC_COMMA, KC_DOT, COMBO_END};
 const uint16_t PROGMEM m_comm[] = {KC_M,KC_COMMA,  COMBO_END};
 const uint16_t PROGMEM n_m[] = {KC_N, KC_M,COMBO_END};
@@ -26,10 +26,10 @@ const uint16_t PROGMEM o_p_combo[]= {KC_O,KC_P, COMBO_END};
 combo_t key_combos[COMBO_COUNT] = {
     [JK_ESC] = COMBO(jk_combo, KC_ESC),
     [YU_COM] = COMBO(yu_combo, KC_CIRC),
-    [UI_COM] = COMBO(ui_combo, KC_DLR),
+    [UI_COM] = COMBO(ui_combo, KC_CIRC),
     [IO_COM] = COMBO(io_combo, KC_TILD),
     [QW_COM] = COMBO(qw_combo, KC_AT),
-    [COM_SLS] = COMBO(com_sls, KC_QUES),
+    [DOT_SLS] = COMBO(dot_sls, KC_TILD),
     [COM_DOT] = COMBO(com_dot, KC_QUES),
     [M_COMM] = COMBO(m_comm, KC_ESC),
     [N_M] = COMBO(n_m, KC_DLR),
--- users/danielo515/config.h
+++ users/danielo515/config.h
@@ -2,7 +2,7 @@
 
 #if defined(COMBO_ENABLE)
   #define COMBO_COUNT 10
-  #define COMBO_TERM  40
+  #define COMBO_TERM  25
 #endif // !COMBO_ENABLE
 // Timeout settings for leader key
 #undef  LEADER_TIMEOUT
