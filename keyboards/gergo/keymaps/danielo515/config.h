a28245acb06efd674a45ae7bce49738edb523066
Danielo Rodriguez <rdanielo@gmail.com>
2020-08-25 18:51:29 +0200

    chore: danielo gergo first commit

---
+++ keyboards/gergo/keymaps/danielo515/config.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define IGNORE_MOD_TAP_INTERRUPT
+#define COMBO_ALLOW_ACTION_KEYS
+#define COMBO_VARIABLE_LEN
--- users/danielo515/rules.mk
+++ users/danielo515/rules.mk
@@ -3,7 +3,7 @@ SRC +=  danielo515.c \
 		process_records.c
 
 ifeq ($(strip $(COMBO_ENABLE)), yes)
-    SRC += combo.c
+	VPATH += keyboards/gboards/
 endif
 
 ifeq ($(strip $(TAP_DANCE_ENABLE)), yes)
